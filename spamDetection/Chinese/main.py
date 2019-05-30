f37c6c827c74392e842b70634c025713eaf3146a
radish608 <randolph_1997@163.com>
2019-05-30 10:49:45 +0800

    spam MLP

--- spamDetection/Chinese/main.py
+++ spamDetection/Chinese/main.py
@@ -1,56 +1,151 @@
-# encoding: utf-8
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_selection import *
-from sklearn import svm, ensemble, feature_selection
-from sklearn.model_selection import train_test_split, cross_validate
-from sklearn.utils import shuffle
-import PreProcess
+from sklearn.feature_extraction.text import CountVectorizer
+import os
+from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
 import numpy as np
+from sklearn import feature_selection
+from sklearn.feature_extraction.text import TfidfTransformer
+import tensorflow as tf
+import tflearn
+from sklearn.feature_selection import *
+from sklearn import ensemble, feature_selection
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.utils import shuffle
+from sklearn import preprocessing
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_1d, global_max_pool
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.merge_ops import merge
+from tflearn.layers.estimator import regression
+from tflearn.data_utils import to_categorical, pad_sequences
+from sklearn.neural_network import MLPClassifier
+from tflearn.layers.normalization import local_response_normalization
+from tensorflow.contrib import learn
+import preprocess_svm
+import preprocess
+import joblib
+
+max_features=5000
+max_document_length=100
+
+def show_diffrent_max_features():
+    global max_features
+    a=[]
+    b=[]
+    for i in range(1000,20000,2000):
+        max_features=i
+        print "max_features=%d" % i
+        x, y = get_features_by_wordbag()
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
+        gnb = GaussianNB()
+        gnb.fit(x_train, y_train)
+        y_pred = gnb.predict(x_test)
+        score=metrics.accuracy_score(y_test, y_pred)
+        a.append(max_features)
+        b.append(score)
+        plt.plot(a, b, 'r')
+    plt.xlabel("max_features")
+    plt.ylabel("metrics.accuracy_score")
+    plt.title("metrics.accuracy_score VS max_features")
+    plt.legend()
+    plt.show()
 
 
-class Classifier(object):
-    def __init__(self, classifier=LogisticRegression):
-        self.classifier = classifier
-        self.clf = None
+def do_cnn(trainX, testX, trainY, testY):
+    global max_document_length
 
-    def train_func(self, x_data, y_data):
-        train_model = self.classifier(class_weight='balanced')
-        print x_data.shape
-        self.clf = train_model.fit(x_data, y_data)
-        return train_model
+    trainX = pad_sequences(trainX, maxlen=max_document_length, value=0.)
+    testX = pad_sequences(testX, maxlen=max_document_length, value=0.)
 
-def result_vectoring(v):
-    v = v.reshape(-1, 1)
-    return np.concatenate((v*(-1)+1, v), axis=1)
+    trainY = to_categorical(trainY, nb_classes=2)
+    testY = to_categorical(testY, nb_classes=2)
+
+    # Building convolutional network
+    network = input_data(shape=[None,max_document_length], name='input')
+    network = tflearn.embedding(network, input_dim=1000000, output_dim=128)
+    branch1 = conv_1d(network, 128, 3, padding='valid', activation='relu', regularizer="L2")
+    branch2 = conv_1d(network, 128, 4, padding='valid', activation='relu', regularizer="L2")
+    branch3 = conv_1d(network, 128, 5, padding='valid', activation='relu', regularizer="L2")
+    network = merge([branch1, branch2, branch3], mode='concat', axis=1)
+    network = tf.expand_dims(network, 2)
+    network = global_max_pool(network)
+    network = dropout(network, 0.8)
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate=0.001,
+                         loss='categorical_crossentropy', name='target')
+    # Training
+    model = tflearn.DNN(network, tensorboard_verbose=0)
+    model.fit(trainX, trainY,
+              n_epoch=5, shuffle=True, validation_set=(testX, testY),
+              show_metric=True, batch_size=100,run_id="spam")
+
+def do_rnn(trainX, testX, trainY, testY):
+    global max_document_length
+    trainX = pad_sequences(trainX, maxlen=max_document_length, value=0.)
+    testX = pad_sequences(testX, maxlen=max_document_length, value=0.)
+    # Converting labels to binary vectors
+    trainY = to_categorical(trainY, nb_classes=2)
+    testY = to_categorical(testY, nb_classes=2)
+
+    # Network building
+    net = tflearn.input_data([None, max_document_length])
+    net = tflearn.embedding(net, input_dim=10240000, output_dim=128)
+    net = tflearn.lstm(net, 128, dropout=0.8)
+    net = tflearn.fully_connected(net, 2, activation='softmax')
+    net = tflearn.regression(net, optimizer='adam', learning_rate=0.1,
+                             loss='categorical_crossentropy')
+
+    # Training
+    model = tflearn.DNN(net, tensorboard_verbose=0)
+    model.fit(trainX, trainY, validation_set=(testX, testY), show_metric=True,
+              batch_size=10,run_id="spm-run",n_epoch=5)
+
+
+def do_dnn(x_train, x_test, y_train, y_test):
+    # MLP
+    clf = MLPClassifier(solver='lbfgs',
+                        alpha=1e-5,
+                        hidden_layer_sizes = (5, 2),
+                        random_state = 1)
+    print  clf
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    print metrics.accuracy_score(y_test, y_pred)
+    print metrics.confusion_matrix(y_test, y_pred)
+
 
 
 def feature_select(X, y):
     return SelectKBest(chi2, k=500).fit_transform(X, y)
 
-
 if __name__ == "__main__":
+    #x,y=get_features_by_wordbag()
+    #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.4, random_state = 0)
+
+    #print "get_features_by_wordbag_tfidf"
+    #x,y=get_features_by_wordbag_tfidf()
+
+    #show_diffrent_max_features()
+
+    #print "get_features_by_tf"
+    #x,y=get_features_by_tf()
+    #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.4, random_state = 0)
+    #CNN
+    #do_cnn_wordbag(x_train, x_test, y_train, y_test)
     ham = './ham_all.txt'
     spam = './spam_all.txt'
-    models = [LogisticRegression, ensemble.RandomForestClassifier, svm.LinearSVC]
 
-    pre_process = 0
-    is_test = 1
-    if pre_process:
-        PreProcess.process_data([ham, spam])  # 处理原始数据，tf-idf训练后并保存
-    if is_test:
-        x, y, model = PreProcess.load_data()  # 加载tf-idf数据
-        x = feature_select(x, y)
-        #print len(model.get_feature_names())
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.9)
-        print x.shape
-        #model = Classifier()
-        #model.train_func(x_train, y_train)
-        #y_pred = model.clf.predict(x_test)
-        #y_vec = result_vectoring(y_test)
-        #y_score = model.clf.predict_proba(x_test)
-        #print 'roc_auc_score:', metrics.roc_auc_score(y_vec, y_score)
-        #print 'classification_report\n', metrics.classification_report(y_test, y_pred)
+    #preprocess_svm.process_data([ham, spam])
+
+    #x, y, vectoring = preprocess_svm.load_data()
+    x, y = preprocess.get_features_by_tfidf()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.4, random_state = 0)
+    do_dnn(x_train, x_test, y_train, y_test)
+    #x = feature_select(x, y)
+    #x = x.toarray()
+
+    do_cnn(x_train, x_test, y_train, y_test)
+
+    #RNN
+    #do_rnn_wordbag(x_train, x_test, y_train, y_test)
--- spamDetection/Chinese/preprocess.py
+++ spamDetection/Chinese/preprocess.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+
 from sklearn.feature_extraction.text import CountVectorizer
 import os
 from sklearn.naive_bayes import GaussianNB
@@ -6,9 +7,9 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import svm
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
 import jieba
+import tflearn
 
 max_features=5000
 max_document_length=100
@@ -19,43 +20,43 @@ def segment(line):
     '''
     return list(jieba.cut(line))
 
-def load_one_file(filename):
-    x=""
-    with open(filename) as f:
-        for line in f:
-            line = line.strip('\n')
-            line = line.strip('\r')
-            x+=line
-    return x
+def get_features_by_tfidf():
+    global  max_document_length
+    file_list = ['./ham_5000.utf8', './spam_5000.utf8']
 
-def load_all_files():
-    ham=[]
-    spam=[]
-    ham = load_files_from_dir('./ham_all.txt')
-    spam = load_files_from_dir('./spam_all.txt')
-    return ham,spam
+    content = []
+    file_cnt = []
+    for file_name in file_list:
+        before_size = len(content)
+        content.extend(open(file_name,'r').readlines())
+        file_cnt.append(len(content)-before_size)
+    """
+    vp=tflearn.data_utils.VocabularyProcessor(max_document_length=100,
+                                                  min_frequency=0,
+                                                  vocabulary=None,
+                                                  tokenizer_fn=segment)
+    """
 
-def get_features_by_wordbag():
+    vectoring = TfidfVectorizer(input='content', tokenizer=segment, analyzer='word')
+    x = vectoring.fit_transform(content)
+    y = np.concatenate((np.repeat([1], file_cnt[0],axis=0),
+                        np.repeat([0], file_cnt[1], axis=0)), axis=0)
+
+    x = x.toarray()
+    return x, y
+
+
+def get_features_by_tf():
+    global max_document_length
+    x=[]
+    y=[]
     ham, spam=load_all_files()
     x=ham+spam
     y=[0]*len(ham)+[1]*len(spam)
-
-    vectoring = TfidfVectorizer(input='content', tokenizer=segment, analyzer='word')
-
-    x = vectoring.fit_transform(x)
-
-
-
-
-
-    vectorizer = CountVectorizer(
-                                 decode_error='ignore',
-                                 strip_accents='ascii',
-                                 max_features=max_features,
-                                 stop_words='english',
-                                 max_df=1.0,
-                                 min_df=1 )
-    print vectorizer
-    x=vectorizer.fit_transform(x)
-    x=x.toarray()
+    vp=tflearn.data_utils.VocabularyProcessor(max_document_length=max_document_length,
+                                              min_frequency=0,
+                                              vocabulary=None,
+                                              tokenizer_fn=None)
+    x=vp.fit_transform(x, unused_y=None)
+    x=np.array(list(x))
     return x,y
